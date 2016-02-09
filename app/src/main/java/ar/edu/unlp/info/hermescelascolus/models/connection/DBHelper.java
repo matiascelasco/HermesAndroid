cffe4f4a560e6073f6332b793357b88a4f8d96b1
Laura Lus <mlaura.lus@gmail.com>
2016-02-09 17:44:48 -0300

    db

--- app/src/main/java/ar/edu/unlp/info/hermescelascolus/models/connection/DBHelper.java
+++ app/src/main/java/ar/edu/unlp/info/hermescelascolus/models/connection/DBHelper.java
@@ -8,17 +8,43 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Facu on 04/02/2016.
  */
 public class DBHelper extends SQLiteOpenHelper {
+    //for Singleton purposes
+    private static DBHelper sInstance;
 
-    public DBHelper(Context context) {
-        super(context, "celascolus.db", null, 1);
-}
+    private static final String DATABASE_NAME = "celascolus.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    private void createCategoriesTable(SQLiteDatabase db) {
+        //creation of categories
+        db.execSQL("CREATE TABLE Categories ("+
+                "ID INTEGER PRIMARY KEY NOT NULL,"+
+                "name VARCHAR(20) NOT NULL" + ");");
+    }
+
+    public static synchronized DBHelper getInstance(Context context) {
+        // Use the application context, which will ensure that you
+        // don't accidentally leak an Activity's context.
+        if (sInstance == null) {
+            sInstance = new DBHelper(context.getApplicationContext());
+        }
+        return sInstance;
+    }
+
+    private DBHelper(Context context) {
+        //the constructor remains private for singleton implementation
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        //
+        //this method must execute if the database file does not exists
+        this.createCategoriesTable(db);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        //
+        //this method must execute if the database file exists, but the version is greater
+        //than the new version
     }
 }
---
+++ app/src/main/java/ar/edu/unlp/info/hermescelascolus/models/dao/CategoryDAO.java
@@ -0,0 +1,22 @@
+package ar.edu.unlp.info.hermescelascolus.models.dao;
+
+import ar.edu.unlp.info.hermescelascolus.models.Category;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+/**
+ * Created by laura on 09/02/16.
+ */
+public class CategoryDAO implements Dao<Category> {
+
+    private SQLiteDatabase db;
+
+    public ArrayList<Category> all(){
+        return null;
+    }
+
+    public Category getById(int id){
+        return null;
+    }
+}
