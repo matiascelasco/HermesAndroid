b6d74a30bac0f56ea857ee68f24514063dda0c5c
Laura Lus <mlaura.lus@gmail.com>
2016-02-09 18:02:35 -0300

    changes for DBHelper

--- app/src/main/java/ar/edu/unlp/info/hermescelascolus/models/connection/DBHelper.java
+++ app/src/main/java/ar/edu/unlp/info/hermescelascolus/models/connection/DBHelper.java
@@ -8,9 +8,10 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Facu on 04/02/2016.
  */
 public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "celascolus.db";
 
     public DBHelper(Context context) {
-        super(context, "celascolus.db", null, 1);
+        super(context, DATABASE_NAME, null, 1);
 }
     @Override
     public void onCreate(SQLiteDatabase db) {
