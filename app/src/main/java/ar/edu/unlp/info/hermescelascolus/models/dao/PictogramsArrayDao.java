ce99be9298a47eb32a55ca9af8bfdcf8695e3bea
Laura Lus <mlaura.lus@gmail.com>
2016-02-15 22:32:15 -0300

    category in the manual load of pictograms

--- app/src/main/java/ar/edu/unlp/info/hermescelascolus/models/dao/PictogramsArrayDao.java
+++ app/src/main/java/ar/edu/unlp/info/hermescelascolus/models/dao/PictogramsArrayDao.java
@@ -10,96 +10,105 @@ import ar.edu.unlp.info.hermescelascolus.models.Pictogram;
 public class PictogramsArrayDao implements Dao<Pictogram> {
 
     private static Integer[] imageIds = {
+            R.drawable.no,
+            R.drawable.si,
+            //needs
             R.drawable.banio,
-            R.drawable.dolorida,
-            R.drawable.dolorido,
+            R.drawable.sed_ninia,
+            R.drawable.sed_ninio,
             R.drawable.cansada,
             R.drawable.cansado,
+            //emotions
+            R.drawable.dolorida,
+            R.drawable.dolorido,
             R.drawable.triste_ninia,
             R.drawable.triste_ninio,
             R.drawable.sorprendida,
             R.drawable.sorprendido,
-            R.drawable.sed_ninia,
-            R.drawable.sed_ninio,
             R.drawable.asustada,
             R.drawable.asustado,
             R.drawable.contenta,
             R.drawable.contento,
             R.drawable.enojada,
             R.drawable.enojado,
+            //barn
+            R.drawable.caballo,
             R.drawable.casco,
             R.drawable.cepillo,
-            R.drawable.chapas,
+            R.drawable.zanahoria,
             R.drawable.limpieza,
-            R.drawable.letras,
             R.drawable.escarba_vasos,
-            R.drawable.cubos,
-            R.drawable.no,
             R.drawable.montura,
             R.drawable.matra,
-            R.drawable.maracas,
-            R.drawable.palos,
             R.drawable.pasto,
-            R.drawable.pato,
-            R.drawable.pelota,
-            R.drawable.si,
             R.drawable.riendas,
             R.drawable.rasqueta_dura,
             R.drawable.rasqueta_blanda,
+            //track
             R.drawable.caballo,
             R.drawable.caballo,
-            R.drawable.caballo,
+            R.drawable.chapas,
             R.drawable.burbujas,
             R.drawable.broches,
             R.drawable.aro,
-            R.drawable.zanahoria,
+            R.drawable.letras,
+            R.drawable.maracas,
+            R.drawable.palos,
+            R.drawable.cubos,
+            R.drawable.pato,
+            R.drawable.pelota,
             R.drawable.tarima
+
     };
 
     private static Integer[] soundIds ={
+            R.raw.no,
+            R.raw.si,
+            //needs
             R.raw.banio,
-            R.raw.me_duele,
-            R.raw.me_duele,
+            R.raw.sed,
+            R.raw.sed,
             R.raw.cansada,
             R.raw.cansado,
+            //emotions
+            R.raw.me_duele,
+            R.raw.me_duele,
             R.raw.triste,
             R.raw.triste,
             R.raw.sorprendida,
             R.raw.sorprendido,
-            R.raw.sed,
-            R.raw.sed,
             R.raw.asustada,
             R.raw.asustado,
             R.raw.contenta,
             R.raw.contento,
             R.raw.enojada,
             R.raw.enojado,
+            //barn
+            R.raw.caballo,
             R.raw.casco,
             R.raw.cepillo,
-            R.raw.chapas,
+            R.raw.zanahoria,
             R.raw.limpieza,
-            R.raw.letras,
             R.raw.escarba_vasos,
-            R.raw.cubos,
-            R.raw.no,
             R.raw.montura,
             R.raw.matra,
-            R.raw.maracas,
-            R.raw.palos,
             R.raw.pasto,
-            R.raw.pato,
-            R.raw.pelota,
-            R.raw.si,
             R.raw.riendas,
             R.raw.rasqueta_dura,
             R.raw.rasqueta_blanda,
+            //track
             R.raw.caballo,
             R.raw.caballo,
-            R.raw.caballo,
+            R.raw.chapas,
             R.raw.burbujas,
             R.raw.broches,
             R.raw.aro,
-            R.raw.zanahoria,
+            R.raw.letras,
+            R.raw.maracas,
+            R.raw.palos,
+            R.raw.cubos,
+            R.raw.pato,
+            R.raw.pelota,
             R.raw.tarima
     };
 
@@ -108,12 +117,42 @@ public class PictogramsArrayDao implements Dao<Pictogram> {
 
     private static void loadArray(){
         array = new ArrayList<>();
-        for (int i = 0; i < imageIds.length; i++){
+        for (int i = 0; i < 7; i++){
+            Pictogram pictogram = new Pictogram();
+            pictogram.setId(i + 1);
+            pictogram.setImageId(imageIds[i]);
+            pictogram.setSoundId(soundIds[i]);
+            pictogram.setCategory(Category.NECESIDADES);
+
+            array.add(pictogram);
+        }
+
+        for (int i = 7; i < 19; i++){
+            Pictogram pictogram = new Pictogram();
+            pictogram.setId(i + 1);
+            pictogram.setImageId(imageIds[i]);
+            pictogram.setSoundId(soundIds[i]);
+            pictogram.setCategory(Category.EMOCIONES);
+
+            array.add(pictogram);
+        }
+
+        for (int i = 19; i < 31; i++){
             Pictogram pictogram = new Pictogram();
             pictogram.setId(i + 1);
             pictogram.setImageId(imageIds[i]);
             pictogram.setSoundId(soundIds[i]);
             pictogram.setCategory(Category.ESTABLO);
+
+            array.add(pictogram);
+        }
+
+        for (int i = 31; i < 43; i++){
+            Pictogram pictogram = new Pictogram();
+            pictogram.setId(i + 1);
+            pictogram.setImageId(imageIds[i]);
+            pictogram.setSoundId(soundIds[i]);
+            pictogram.setCategory(Category.PISTA);
 
             array.add(pictogram);
         }
