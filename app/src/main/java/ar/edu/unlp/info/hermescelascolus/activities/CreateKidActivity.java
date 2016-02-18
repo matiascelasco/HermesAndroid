e14da0a19326b01cf66d101aabbb5e92ecd63c60
Matías Celasco <matiascelasco@gmail.com>
2016-02-18 07:05:06 -0300

    CreateKidActivity done

--- app/src/main/java/ar/edu/unlp/info/hermescelascolus/activities/CreateKidActivity.java
+++ app/src/main/java/ar/edu/unlp/info/hermescelascolus/activities/CreateKidActivity.java
@@ -1,5 +1,6 @@
 package ar.edu.unlp.info.hermescelascolus.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.Toast;
 
 import ar.edu.unlp.info.hermescelascolus.R;
 import ar.edu.unlp.info.hermescelascolus.ValidationError;
+import ar.edu.unlp.info.hermescelascolus.models.Kid;
+import ar.edu.unlp.info.hermescelascolus.models.dao.Daos;
 
 public class CreateKidActivity extends AppCompatActivity {
 
@@ -20,11 +23,11 @@ public class CreateKidActivity extends AppCompatActivity {
         Button btn = (Button) findViewById(R.id.create_kid_button);
         btn.setOnClickListener(new Button.OnClickListener(){
 
-            private String getStringFromEditTextAndValidate(int id, String requiredMsg) throws ValidationError {
+            private String getStringFromEditTextAndValidate(int id, int requiredMsgId) throws ValidationError {
                 EditText widget = (EditText) findViewById(id);
                 String string = widget.getText().toString();
                 if (string.equals("")){
-                    throw new ValidationError(requiredMsg);
+                    throw new ValidationError(getResources().getString(requiredMsgId));
                 }
                 return string;
             }
@@ -34,16 +37,28 @@ public class CreateKidActivity extends AppCompatActivity {
                 try {
                     String firstName = getStringFromEditTextAndValidate(
                             R.id.first_name_input,
-                            "R.string.first_name_required_msg");
+                            R.string.first_name_required_msg);
                     String lastName = getStringFromEditTextAndValidate(
                             R.id.last_name_input,
-                            "R.string.last_name_required_msg");
+                            R.string.last_name_required_msg);
+                    Kid kid = new Kid();
+                    kid.setName(firstName);
+                    kid.setSurname(lastName);
+                    Daos.KID.save(kid);
+                    startKidActivity(kid);
                 } catch (ValidationError validationError) {
                     Toast.makeText(CreateKidActivity.this,
                             validationError.getMessage(), Toast.LENGTH_SHORT).show();
                 }
+
             }
         });
+    }
+
+    private void startKidActivity(Kid kid){
+        Intent intent = new Intent(this, KidActivity.class);
+        intent.putExtra(InitialActivity.KID_ID, kid.getId());
+        startActivity(intent);
     }
 
 
--- app/src/main/java/ar/edu/unlp/info/hermescelascolus/activities/InitialActivity.java
+++ app/src/main/java/ar/edu/unlp/info/hermescelascolus/activities/InitialActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 import ar.edu.unlp.info.hermescelascolus.models.Kid;
 import ar.edu.unlp.info.hermescelascolus.R;
 import ar.edu.unlp.info.hermescelascolus.models.connection.KidSeeder;
+import ar.edu.unlp.info.hermescelascolus.models.dao.Daos;
 import ar.edu.unlp.info.hermescelascolus.models.dao.KidDao;
 
 public class InitialActivity extends AppCompatActivity {
