62b6ecb5c1744604799e814beb2d1914b201b676
Laura Lus <mlaura.lus@gmail.com>
2016-02-20 20:36:24 -0300

    Daos initialize now seed the database

--- app/src/main/java/ar/edu/unlp/info/hermescelascolus/models/dao/Daos.java
+++ app/src/main/java/ar/edu/unlp/info/hermescelascolus/models/dao/Daos.java
@@ -10,7 +10,7 @@ import ar.edu.unlp.info.hermescelascolus.models.connection.PictogramSeeder;
 
 public class Daos {
     public static Dao<Kid> KID;
-   // public static Dao<Pictogram> PICTOGRAM;
+    //public static Dao<Pictogram> PICTOGRAM;
 
     public static void initialize(Context context){
         if (KID == null){
@@ -19,7 +19,7 @@ public class Daos {
             context.deleteDatabase(DBHelper.DATABASE_NAME);
             KidSeeder.seed(context);
             PictogramSeeder.seed(context);
-           //PICTOGRAM = new PictogramDao(context);
+           // PICTOGRAM = new PictogramDao(context);
         }
     }
     // The order is important!
